e1903137950b038e0dea902bff08d3b2c31760cc
ANCAKE <113901391+Ancake@users.noreply.github.com>
2024-04-09 12:33:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,14 @@ for(let i=0;i <botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-03-26T12:10:00");
+
+const tempoObjetivo1 = new Date("2024-05-03T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
@@ -22,7 +29,6 @@ contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 function atualizarTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
@@ -32,6 +38,26 @@ function atualizarTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
+}
+
+    contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+
+function atualizaCronometro(){
+
+for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000)
 }
